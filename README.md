7b4acc5dcb23dfe2bddcaea2dcdfabb05de20b12
George Elias Ferreira da Silva <fs.georgee@gmail.com>
2019-07-22 22:59:04 -0400

    Leave the gun, take the cannoli.

---
+++ README.md
@@ -0,0 +1,11 @@
+* Pre-requisits
+
+pip install flask-restful
+
+** If using nginx:
+
+pip install uwsgi
+
+
+sudo apt-get install uwsgi-plugin-python
+sudo apt-get install uwsgi uwsgi-emperor
---
+++ dudewheresmycar.py
@@ -0,0 +1,56 @@
+# curl http://parking.mob6.com.br/park -d "floor=3&location=The One&spot=None" -X POST -v
+# curl http://parking.mob6.com.br/parked
+# http://flask-restful.readthedocs.io/en/latest/quickstart.html
+
+import json
+
+from flask import Flask, request, Response
+from flask_restful import Resource, Api, reqparse
+
+class Persistance():
+    def __init__(self):
+        self.filename = '/var/www/mob6/dudewheresmycar.json'
+
+    def save(self, park):
+        with open(self.filename, 'w') as perFile:
+            perFile.write(json.dumps(park))
+
+    def open(self):
+        with open(self.filename, 'r') as perFile:
+            return perFile.read()
+
+class Parked(Resource):
+    def get(self):
+        park = json.loads(persistance.open())
+        response = Response(
+            response='<html><title>Car is parked at</title><style> tab1 { padding-left: 4em; }</style><body><p><tab1>Floor: %s</tab1></p><p><tab1>Location: %s</tab1></p><p><tab1>Spot: %s</tab1></p></body></html>' % (park['floor'], park['location'], park['spot']),
+            #json.dumps(park),
+            status=200)
+        return response
+
+class Park(Resource):
+    def post(self):
+        args = parser.parse_args()
+        park = {}
+        park['floor'] = args['floor']
+        park['location'] = args['location']
+        park['spot'] = args['spot']
+        persistance.save(park)
+        return "OK", 201
+
+application = Flask(__name__)
+api = Api(application)
+
+parser = reqparse.RequestParser()
+parser.add_argument('floor')
+parser.add_argument('location')
+parser.add_argument('spot')
+
+persistance = Persistance()
+
+api.add_resource(Park, '/park')
+api.add_resource(Parked, '/parked')
+
+if __name__ == '__main__':
+    application.run(debug=True)
+
